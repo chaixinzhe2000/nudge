09147f0a0154598b8838859f6476587d637ace91
elliotjinoohong <elliot_hong@brown.edu>
2021-04-14 00:14:53 -0400

    ready to pull

--- components/AddContactModal.tsx
+++ components/AddContactModal.tsx
@@ -1,13 +1,44 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import React from "react";
+import React, { useState } from "react";
 import { Modal, View, Text, StyleSheet } from "react-native";
-
+import * as firebase from 'firebase'
+import 'firebase/firestore';
 interface IAddContactModalProps {
   addContactModalOpen: boolean,
   setAddContactModalOpen: any,
 }
 
 export default function AddContactModal(props: IAddContactModalProps) {
+
+const [newContactEmail, setNewContactEmail] = useState('');
+
+  async function handleSubmit() {
+    const user = firebase.auth().currentUser;
+
+    if (user) {
+      // // get user from db
+      // const userSearchQuery = dbh.collection('User').doc(user.uid);
+      // const userDoc = await userSearchQuery.get();
+      // if (!userDoc.exists) {
+      //   console.log("userDoc does not exist");
+      // } else {
+      //   console.log(userDoc.data);
+      // }
+
+      // // find receiver
+
+
+      const toSend = {
+        newContactEmail: newContactEmail,
+        currentUserEmail: user.email
+      }
+      
+      dbh.collection("Task").add(toSend)
+    } else {
+      alert('Not logged in, please login again.');
+    }
+
+  }
 
   return (
     <Modal visible={props.addContactModalOpen} animationType='slide'>
