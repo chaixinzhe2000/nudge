fcaf343e80fe89f56cfc7f568e7421f168c4eed9
Xinzhe Chai <xinzhe_chai@brown.edu>
2021-04-14 18:31:02 -0400

    styled

--- components/AddContactModal.tsx
+++ components/AddContactModal.tsx
@@ -4,6 +4,11 @@ import { Modal, View, Text, StyleSheet, TextInput, TouchableOpacity } from "reac
 import * as firebase from 'firebase'
 import 'firebase/firestore';
 import "firebase/functions";
+import { Feather } from '@expo/vector-icons';
+
+function FeatherIcon(props: { name: React.ComponentProps<typeof Feather>['name']; color: string }) {
+	return <Feather size={22} style={{ marginTop: -1 }} {...props} />;
+}
 
 interface IAddContactModalProps {
   addContactModalOpen: boolean,
@@ -14,15 +19,12 @@ export default function AddContactModal(props: IAddContactModalProps) {
 
   const [newContactEmail, setNewContactEmail] = useState('');
 
-
   var addContact = firebase.functions().httpsCallable('addContact');
-
 
   async function handleSubmit() {
     const user = firebase.auth().currentUser;
 
     if (user) {
-
       if (user.email === newContactEmail) {
         setNewContactEmail('');
         alert('You cannot add yourself as a contact. Please try again.');
@@ -64,18 +66,23 @@ export default function AddContactModal(props: IAddContactModalProps) {
   return (
     <Modal visible={props.addContactModalOpen} animationType='slide'>
       <View style={styles.modalContent}>
-        <MaterialIcons
-          name='close'
-          size={24}
-          onPress={() => props.setAddContactModalOpen(false)}
-          style={styles.closeButton}
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setNewContactEmail}
-          value={newContactEmail}
-          placeholder={'enter email to find contact'}
-        />
+	  	<TouchableOpacity onPress={() => props.setAddContactModalOpen(false)} >
+          <View style={styles.closeButton}>
+		  	<FeatherIcon name="x" color='#2cb9b0' />
+            <Text style={styles.close}>CLOSE</Text>
+          </View>
+    	</TouchableOpacity>
+		<View style={styles.inputDiv}>
+			<Text style={styles.title}>Find a Nudge pal!</Text>
+        	<TextInput
+				placeholderTextColor='#f9f7f7' textAlign='left'
+          		style={styles.input}
+          		onChangeText={setNewContactEmail}
+          		value={newContactEmail}
+          		placeholder={'hello@nudge.com'}
+        	/>
+			<Text style={styles.subtitle}>EMAIL</Text>
+		</View>
         <TouchableOpacity onPress={() => handleSubmit()} >
           <View style={styles.buttonDiv}>
             <Text style={styles.text}>Add Contact</Text>
@@ -90,32 +97,70 @@ const styles = StyleSheet.create({
   modalContent: {
     flex: 1,
     alignItems: 'flex-start',
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
+	marginTop: 60
   },
   closeButton: {
-    marginTop: 30
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'flex-end',
+	width: '100%',
+	paddingRight: 20
+  },
+  close: {
+	fontSize: 18,
+    fontWeight: '700'
+  },
+  inputDiv: {
+	display: 'flex',
+	flexDirection: 'column',
+	marginLeft: 25,
+	width: '100%',
+	marginTop: 40,
+	minHeight: 140
   },
   input: {
     height: 40,
-    borderWidth: 1,
-    width: '95%'
+    width: '88%',
+	marginTop: 15,
+	backgroundColor: '#c4c4c4',
+	borderRadius: 10,
+	minHeight: 50,
+	fontSize: 18,
+	paddingLeft: 20,
+	fontWeight: '600',
+	color: 'white'
+  },
+  title: {
+	fontWeight: '700',
+	fontSize: 22,
+	paddingLeft: 8
+  },
+  subtitle: {
+	fontWeight: '700',
+	fontSize: 15,
+	paddingTop: 8,
+	paddingLeft: 8,
+	color: '#2cb9b0'
   },
   buttonDiv: {
-    backgroundColor: 'white',
-    minHeight: 50,
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderRadius: 10,
-    marginBottom: 12,
-    paddingRight: 10
+	  width: '100%',
+	  minWidth: 345,
+	backgroundColor: '#2cb9b0',
+		minHeight: 50,
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		flexDirection: 'row',
+		borderRadius: 10,
+		marginBottom: 12,
+		marginLeft: 25,
+		paddingLeft: 20
   },
   text: {
-    marginLeft: 15,
-    color: '#2cb9b0',
+    color: 'white',
     fontSize: 18,
     fontWeight: '700'
-  }
+  },
 })
 
