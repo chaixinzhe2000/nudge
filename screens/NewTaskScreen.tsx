23c5c1b1aecbca7c4640d88a95b75767bb3fa737
zhixingj <69444736+zhixingj@users.noreply.github.com>
2021-04-14 21:12:25 -0500

    new task works

--- screens/NewTaskScreen.tsx
+++ screens/NewTaskScreen.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Platform, SafeAreaView, StyleSheet, TextInput, View, Text, TouchableOpacity, Image } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, TextInput, View, Text, TouchableOpacity, Image, ActionSheetIOS } from 'react-native';
 import { useState, useEffect } from 'react';
 import * as firebase from 'firebase'
 import 'firebase/firestore';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { Picker } from '@react-native-picker/picker';
 
 interface IContact {
 	uid: string,
@@ -18,13 +19,12 @@ export default function NewTaskScreen() {
 
 	const [taskName, setTaskName] = useState("");
 	const [extraDetails, setExtraDetails] = useState("");
-	const [receiverName, setReceiverName] = useState("");
 	const [location, setLocation] = useState("");
-	const [priority, setPriority] = useState("");
+	const [priority, setPriority] = useState("medium");
 	const [receiverUid, setReceiverUid] = useState("");
 	const [contactList, setContactList]: [IContact[], any] = useState([])
 	// data stuff
-	const [date, setDate] = useState(new Date());
+	const [date, setDate]: [Date, any] = useState(new Date());
 	const [show, setShow] = useState(true);
 
 	const onChange = (event, selectedDate) => {
@@ -36,14 +36,17 @@ export default function NewTaskScreen() {
 	const db = firebase.firestore();
 
 	var getContacts = firebase.functions().httpsCallable('getContacts');
+	var addTask = firebase.functions().httpsCallable('addTask');
 
 	useEffect(() => {
 		async function getContactsCaller() {
-		  await getContacts();
+			let contactResponse = await getContacts();
+			if (contactResponse.data.status) {
+				setContactList(contactResponse.data.contacts)
+			}
 		}
 		getContactsCaller();
-		console.log("contactsList");
-	  },[])
+	}, [])
 
 	async function handleSubmit() {
 		const user = firebase.auth().currentUser;
@@ -54,27 +57,55 @@ export default function NewTaskScreen() {
 				due: date,
 				location: location,
 				priority: priority,
-				receiverUid: receiverUid
+				receiverUid: receiverUid,
+				extraDetails: extraDetails
 			}
-
+			console.log(toSend);
+			addTask(toSend)
+			.then((res) => {
+				console.log(res)
+			})
+			.catch(console.log);
 		} else {
 			alert('Not logged in, please login again.');
 		}
 	}
 
-	const handleSelectContact = (e) => {
-		setReceiverUid(e.target.getAttribute('uid'))
+	const handleSelectContact = (uid: string) => {
+		setReceiverUid(uid);
+		console.log(receiverUid);
 	}
 
-
 	const contactListElement = contactList.map((contact: IContact) =>
-		<TouchableOpacity onPress={e => {handleSelectContact(e)}} key={contact.uid}>
+		<TouchableOpacity onPress={() => { handleSelectContact(contact.uid) }} key={contact.uid}>
+			<Text>{contact.displayName}</Text>
 			<Image
 				style={styles.profileImage}
-				source={{ uri: contact.uid ? contact.uid : 'https://i.pinimg.com/originals/5d/70/18/5d70184dfe1869354afe7bf762416603.jpg' }}
+				source={{ uri: contact.avatar ? contact.avatar : 'https://i.pinimg.com/originals/5d/70/18/5d70184dfe1869354afe7bf762416603.jpg' }}
 			/>
 		</TouchableOpacity>
 	)
+
+	const onPress = () =>
+		ActionSheetIOS.showActionSheetWithOptions(
+			{
+				options: ["Cancel", "Low", "Medium", "High"],
+				destructiveButtonIndex: 3,
+				cancelButtonIndex: 0,
+			},
+			buttonIndex => {
+				if (buttonIndex === 0) {
+					setPriority('medium')
+				} else if (buttonIndex === 1) {
+					setPriority('low');
+				} else if (buttonIndex === 2) {
+					setPriority('medium');
+				} else {
+					setPriority('high')
+				}
+			}
+		);
+
 	return (
 		<SafeAreaView style={styles.mainContainer}>
 			<TextInput
@@ -85,7 +116,10 @@ export default function NewTaskScreen() {
 			/>
 			<View>
 				<Text>List of Contacts</Text>
-			{contactListElement}
+				<View style={styles.contactList}>
+					{contactListElement}
+				</View>
+
 			</View>
 			<View>
 				<TextInput
@@ -106,16 +140,16 @@ export default function NewTaskScreen() {
 					placeholder="Priority"
 					value={priority}
 				/>
+				<Button onPress={onPress} title="Set Priority" />
 				<Text>Select Due Date: </Text>
-				{show && (
-					<DateTimePicker
-						testID="dateTimePicker"
-						value={date}
-						mode={'datetime'}
-						display="default"
-						onChange={onChange}
-					/>
-				)}
+				<DateTimePicker
+					testID="dateTimePicker"
+					value={date}
+					mode={'datetime'}
+					display="default"
+					onChange={onChange}
+				/>
+
 			</View>
 			<Button
 				icon={
@@ -128,7 +162,6 @@ export default function NewTaskScreen() {
 				buttonStyle={styles.submitButton}
 				title="Send"
 				onPress={() => handleSubmit()}
-
 			/>
 		</SafeAreaView>
 	);
@@ -139,7 +172,7 @@ const styles = StyleSheet.create({
 		display: 'flex',
 		flex: 1,
 		width: '100%',
-		justifyContent: 'space-between',
+		justifyContent: 'flex-start',
 		borderColor: 'blue',
 		borderWidth: 3,
 	},
@@ -162,4 +195,8 @@ const styles = StyleSheet.create({
 		marginRight: 10,
 		borderRadius: 90
 	},
+	contactList: {
+		display: 'flex',
+		flexDirection: 'row'
+	}
 });
